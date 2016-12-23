ee31fcaee461165223ad6e4c2140470f61b81efb
Alua.Kinzhebayeva <Alua.Kinzhebayeva@gmail.com>
2016-12-23 22:25:23 +0600

    forwarding calls

--- LumusMaximaContract.sol
+++ LumusMaximaContract.sol
@@ -1,25 +1,20 @@
 pragma solidity ^0.4.0;
 
 contract LumusMaximaContract {
+    address public currentVersion;
+    address public owner;
 
-  address public owner;
-  mapping (address => uint) public balances;
+    function LumusMaximaContract(address initAddr){
+        currentVersion = initAddr;
+        owner = msg.sender;
+    }
 
-  function LumusMaximaContract() {
-    owner = msg.sender;
-  }
+    function update(address newAddress){
+        if(msg.sender != owner) throw;
+        currentVersion = newAddress;
+    }
 
-  event Sent(address from, address to, uint amount);
-
-  event Print(string out);
-
-  function topUp(uint amount) {}
-
-  function refund(address reciever, uint amount) {}
-
-  function payOff(address from, address to, uint amount) {}
-
-  function () {
-    Print("Huston, we have a contract to write");
-  }
+    function(){
+        if(!currentVersion.delegatecall(msg.data)) throw;
+    }
 }
